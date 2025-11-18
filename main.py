3f78e1690452bb6f8be59eaddd84fa084b68b130
abhishek7532 <aksharma123567@gmail.com>
2025-11-18 23:20:04 +0530

    Fix flake8 errors

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
-    return {"message": "Hello! Welcome to the Greeting API"}
+    return {"message": "Hello, World!"}
+
 
 @app.get("/greet/{name}")
-def greet_user(name: str):
-    return {"greeting": f"Hello {name}, nice to meet you!"}
+def greet(name: str):
+    return {"message": f"Hello, {name}! Welcome to FastAPI world"}
